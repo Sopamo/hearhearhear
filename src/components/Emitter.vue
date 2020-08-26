a10fa239db9ebbe11c86180bff61bd091901e1ff
Paul Mohr <p.mohr@sopamo.de>
2020-08-26 18:01:07 +0200

    add kinda working analyser

--- src/components/Emitter.vue
+++ src/components/Emitter.vue
@@ -17,7 +17,8 @@ export default {
       analyser: null,
       isStarted: false,
       volume: 0.01,
-      frequency: 440,
+      frequency: 0,
+      bits: [],
     };
   },
   mounted() {
@@ -37,7 +38,7 @@ export default {
       this.setVolume(0);
 
       this.oscillator = this.audioCtx.createOscillator();
-      this.oscillator.type = 'square';
+      this.oscillator.type = 'sine';
       this.setFrequency(this.frequency);
       this.oscillator.connect(this.gainNode);
     },
@@ -61,66 +62,93 @@ export default {
       this.oscillator.frequency.setValueAtTime(frequency, this.audioCtx.currentTime);
     },
     setFrequencies() {
-      const bits = [0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1];
+      const text = '127.0.0.1';
+      this.bits = [];
+      for (let i = 0; i < text.length; i += 1) {
+        text[i].charCodeAt(0).toString(2).split('').forEach((bit) => {
+          this.bits.push(parseInt(bit, 10));
+        });
+      }
       // The amount of bits per second
       const density = 10;
       const now = this.audioCtx.currentTime + 1;
-      const low = 440;
-      const high = 523;
-      this.oscillator.frequency.setValueAtTime(200, this.audioCtx.currentTime);
-      bits.forEach((bit, idx) => {
+      const low = 500;
+      const high = 1500;
+      this.oscillator.frequency.setValueAtTime(0, this.audioCtx.currentTime);
+      this.bits.forEach((bit, idx) => {
         let frequency = low;
         if (bit) {
           frequency = high;
         }
         this.oscillator.frequency.setValueAtTime(frequency, now + (idx * (1 / density)));
       });
-      this.oscillator.frequency.setValueAtTime(200, now + (bits.length * (1 / density)));
+      this.oscillator.frequency.setValueAtTime(0, now + (this.bits.length * (1 / density)));
     },
     setupAnalyzer() {
       this.analyser = this.audioCtx.createAnalyser();
       this.analyser.connect(this.audioCtx.destination);
       this.gainNode.connect(this.analyser);
-      this.analyser.fftSize = 2048;
+      this.analyser.fftSize = 512;
+
       const bufferLength = this.analyser.frequencyBinCount;
       const dataArray = new Uint8Array(bufferLength);
-      this.analyser.getByteTimeDomainData(dataArray);
+      this.analyser.getByteFrequencyData(dataArray);
 
       const canvas = document.getElementById('oscilloscope');
       const canvasCtx = canvas.getContext('2d');
 
       const that = this;
+      const outBits = [];
+      let currentPrimaryBin = null;
+      let primaryBinCount = 0;
       const draw = () => {
-        requestAnimationFrame(() => { draw.call(that); });
+        window.setTimeout(() => { draw.call(that); }, 10);
 
-        this.analyser.getByteTimeDomainData(dataArray);
+        this.analyser.getByteFrequencyData(dataArray);
 
         canvasCtx.fillStyle = 'rgb(200, 200, 200)';
         canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
-        canvasCtx.lineWidth = 2;
-        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-        canvasCtx.beginPath();
-
-        const sliceWidth = canvas.width * (1.0 / bufferLength);
+        const barWidth = (canvas.width / bufferLength) * 2.5;
+        let barHeight;
         let x = 0;
-
+        let maxValue = 0;
+        let primaryBin = null;
         for (let i = 0; i < bufferLength; i += 1) {
-          const v = dataArray[i] / 128.0;
-          const y = v * (canvas.height / 2);
-
-          if (i === 0) {
-            canvasCtx.moveTo(x, y);
-          } else {
-            canvasCtx.lineTo(x, y);
+          barHeight = dataArray[i];
+          if (barHeight > maxValue) {
+            maxValue = barHeight;
+            primaryBin = i;
           }
 
-          x += sliceWidth;
-        }
+          canvasCtx.fillStyle = `rgb(${barHeight + 100},50,50)`;
+          canvasCtx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
 
-        canvasCtx.lineTo(canvas.width, canvas.height / 2);
-        canvasCtx.stroke();
+          x += barWidth + 1;
+        }
+        const distanceLow = Math.abs(6 - primaryBin);
+        const distanceHigh = Math.abs(17 - primaryBin);
+        if (distanceLow < 4 || distanceHigh < 4) {
+          console.log(`Primary: ${primaryBin}`);
+          if (primaryBin !== currentPrimaryBin) {
+            if (primaryBinCount > 0) {
+              console.log(`got ${primaryBinCount}`);
+              for (let i = 0; i < Math.round(primaryBinCount / 10); i += 1) {
+                if (distanceLow < distanceHigh) {
+                  outBits.push(1);
+                } else {
+                  outBits.push(0);
+                }
+              }
+
+              console.log(outBits);
+              console.log(Object.values(this.bits));
+            }
+            primaryBinCount = 0;
+          }
+          currentPrimaryBin = primaryBin;
+          primaryBinCount += 1;
+        }
       };
 
       draw();
